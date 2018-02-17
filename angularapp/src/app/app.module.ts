8645ade3f0d08ece22216df9d88aa3fc24d88931
mahesh <mayeeworrld4u@gmail.com>
2018-02-17 18:34:20 +0530

    Removed empty spaces

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { routing } from './app.rounting.module';
