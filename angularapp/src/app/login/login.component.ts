420b2394468dcbeb42141ede56894752ad15e962
mahesh <mayeeworrld4u@gmail.com>
2018-02-24 15:09:05 +0530

    Updated login form

--- angularapp/src/app/login/login.component.ts
+++ angularapp/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  registerUser(form: NgForm) {
+    console.log(form.value);
+    // {email: '...', password: '...'}
+    // ...
+  }
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
