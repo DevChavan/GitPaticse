88abd9a3dab3c28aef1aa8ecbbfe362327d6acd5
mahesh <mayeeworrld4u@gmail.com>
2018-02-25 11:03:58 +0530

    added register component

---
+++ angularapp/src/app/service/user.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { apiConfig } from "../app.config"
+
+@Injectable()
+export class UserService {
+  
+  constructor( private http: HttpClient) {  
+  }
+
+getAll() {
+  return this.http.get(apiConfig.apiUrl+'/users')
+}
+create(user) {
+  return this.http.post(apiConfig.apiUrl+'/users/register', user)
+}
+
+
+}
